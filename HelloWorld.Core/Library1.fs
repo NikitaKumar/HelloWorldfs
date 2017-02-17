9d1c117b600ea153434e00705744315b1c108213
Nikita Kumar <nikitaku@cybage.com>
2017-02-17 11:46:21 +0530

    initial commit

---
+++ HelloWorld.Core/Library1.fs
@@ -0,0 +1,8 @@
+﻿//namespace HelloWorld.Core
+//
+//type Class1() = 
+//    member this.X = "F#"
+namespace HelloWorld.Core
+
+module Hello =
+    let SayHello name = "Hello"
---
+++ HelloWorld.Test/Library1.fs
@@ -0,0 +1,12 @@
+﻿//namespace HelloWorld.Test
+//
+//type Class1() = 
+//    member this.X = "F#"
+module HelloWorld.Tests.Hello
+
+open HelloWorld.Core.Hello
+open NUnit.Framework
+open FsUnit
+
+[<Test>]
+let shouldSayHello () = Assert.AreEqual("Hello World!", SayHello "World")
