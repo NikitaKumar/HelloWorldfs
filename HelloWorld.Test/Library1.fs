e881340351d90f2a028d8fded2ddb3a71941e51b
Nikita Kumar <nikitaku@cybage.com>
2017-02-17 16:21:28 +0530

    ahdjkahdk

--- HelloWorld.Test/Library1.fs
+++ HelloWorld.Test/Library1.fs
@@ -1,16 +1,17 @@
-﻿//namespace HelloWorld.Test
+﻿namespace HelloWorld.Test
 //
 //type Class1() = 
 //    member this.X = "F#"
-module HelloWorld.Tests.Hello
+//module HelloWorld.Tests.Hello
 
 open HelloWorld.Core.Hello
-open HelloWorld.Core.Bye
-open NUnit.Framework
-open FsUnit
+module Hellotest=   
+//    open HelloWorld.Core.Bye
+    open NUnit.Framework
+    open FsUnit
 
-[<Test>]
-let shouldSayHello () = Assert.AreEqual("Hello World!", SayHello "World")
+    [<Test>]
+    let shouldSayHello () = Assert.AreEqual("Hello World!", SayHello "World")
 
-//[<Test>]
-//let shouldSayBye () = Assert.AreEqual("Hello World!", SayBye "World")
+//    [<Test>]
+//    let shouldSayBye () = Assert.AreEqual("Hello World!", SayBye "World")
